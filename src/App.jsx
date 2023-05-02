37fa8ff1c0c87b538817d503d1427d8fbd72ce26
QuanIT01 <n.h.quan126@gmail.com>
2023-05-02 09:41:58 +0700

    zustand

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HackerNews from "./components/HackerNews";
+const App = () => {
+  return (
+    <div className="p-10">
+      <HackerNews></HackerNews>
+    </div>
+  );
+};
+
+export default App;
